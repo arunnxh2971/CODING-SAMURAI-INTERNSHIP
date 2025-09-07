00b52be4874e6a555ce694f3bb1956c4f7ff567f
arunnxh2971 <arun.gh971@gmail.com>
2025-09-07 18:07:26 +0530

    Update HomePage and other files

--- ecomm/frontend/src/App.js
+++ ecomm/frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
 import SignInPage from './pages/SignInPage';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -16,8 +16,10 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/signin" element={<SignInPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
 export default App;
+
+
